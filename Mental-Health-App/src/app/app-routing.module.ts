0891ce98f24363b6aaa886fd6ad502f924fdd522
Jonne Kiukas <jonne.kiukas@hotmail.com>
2020-03-24 16:01:52 +0200

    Relief part initialized. Added out, breathing exercise, crisis phone, and own suggestion buttons.

--- Mental-Health-App/src/app/app-routing.module.ts
+++ Mental-Health-App/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DayTrackingComponent} from "./day-tracking/day-tracking.component";
+import {ReliefComponent} from "./relief/relief.component";
 
 
 const routes: Routes = [
-  {path: 'day-tracking', component: DayTrackingComponent}
+  {path: 'day-tracking', component: DayTrackingComponent},
+  {path: 'relief', component: ReliefComponent }
 ];
 
 @NgModule({
--- Mental-Health-App/src/app/app.module.ts
+++ Mental-Health-App/src/app/app.module.ts
@@ -6,19 +6,23 @@ import { DayTrackingComponent } from './day-tracking/day-tracking.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatSliderModule} from '@angular/material/slider';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ReliefComponent } from './relief/relief.component';
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DayTrackingComponent
+    DayTrackingComponent,
+    ReliefComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatCardModule,
     MatSliderModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
