e851b2675947828a6d81c139f60dfb7c863de709
Jonne Kiukas <jonne.kiukas@hotmail.com>
2020-04-13 15:20:33 +0300

    Added logout button to menu. Also, remove unnecessary comments.

--- Mental-Health-App/src/app/menu/menu.component.ts
+++ Mental-Health-App/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService, public router: Router) { }
 
   ngOnInit(): void {
   }
@@ -16,5 +18,9 @@ export class MenuComponent implements OnInit {
     console.log('uutta raporttia tekemään');
   }
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 }
 //
